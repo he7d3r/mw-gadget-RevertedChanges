39471b0957093be6b123e9d4577ca05133c0fc0b
Helder <he7d3r@gmail.com>
2016-04-01 15:19:52 -0300

    Coding conventions

--- src/RevertedChanges.js
+++ src/RevertedChanges.js
@@ -1,5 +1,6 @@
 /**
  * RevertedChanges
+ *
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
@@ -30,24 +31,24 @@
 			formatversion: 2,
 			continue: ''
 		} ).done( function ( data ) {
-			var revs = data.query.pages[0].revisions,
-				curSha1 = revs[0].sha1,
+			var revs = data.query.pages[ 0 ].revisions,
+				curSha1 = revs[ 0 ].sha1,
 				i;
 			// Find the index of the revision being viewed by the user
-			for( i = 1; i < revs.length; i++ ) {
-				if( revs[i].revid === mw.config.get( 'wgRevisionId' ) ) {
+			for ( i = 1; i < revs.length; i++ ) {
+				if ( revs[ i ].revid === mw.config.get( 'wgRevisionId' ) ) {
 					break;
 				}
 			}
-			if( i + 1 < revs.length && revs[i].sha1 === revs[ i + 1 ].sha1 ) {
+			if ( i + 1 < revs.length && revs[ i ].sha1 === revs[ i + 1 ].sha1 ) {
 				// Ignore page moves, page protections, etc
 				// which don't change the existing content
 				return;
 			}
 			// Check if the latest revision equals to a revision
 			// older than the one being viewed by the user
-			for( i++; i < revs.length; i++ ) {
-				if( revs[i].sha1 === curSha1 ) {
+			for ( i++; i < revs.length; i++ ) {
+				if ( revs[ i ].sha1 === curSha1 ) {
 					// Someone restored an older revision of the page, by
 					// reverting/undoing the one presented in this diff
 					formatDiff();
